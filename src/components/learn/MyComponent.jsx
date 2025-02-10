bfd5e9e75203cd0f9bc13818265cb2ee992d003f
Huy Nguyen <huynguyen@Huys-MacBook-Pro.local>
2025-02-10 13:04:22 +0700

    video 32

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,22 @@
 import "./style.css";
 
 const MyComponent = () => {
+  //   const example = "example"; //string
+  //   const example = 25; // number
+  //   const example = true; // boolean
+  //   const example = undefined; // undefined
+  //   const example = null; // null
+
+  const example = [1, 2, 3]; // array
+  //   const example = {
+  //     name: "test",
+  //     age: 25,
+  //   };
+
   return (
     <>
-      <div>My Component update</div>
+      <div>{JSON.stringify(example)} My Component update</div>
+      <div>{console.log("test")}</div>
       <div className="child" style={{ borderRadius: "10px" }}>
         child
       </div>
