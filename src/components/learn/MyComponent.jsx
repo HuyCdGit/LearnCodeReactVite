bf78d3c1d4f31d4deeb20dc5b629506bff028847
Huy Nguyen <huynguyen@Huys-MacBook-Pro.local>
2025-02-10 12:46:59 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,4 +1,13 @@
+import "./style.css";
+
 const MyComponent = () => {
-  return <div>My Component update</div>;
+  return (
+    <>
+      <div>My Component update</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        child
+      </div>
+    </>
+  );
 };
 export default MyComponent;
