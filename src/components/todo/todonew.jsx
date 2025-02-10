fd517b6b543e555638f3a5331862b3955ef6eac8
Huy Nguyen <huynguyen@Huys-MacBook-Pro.local>
2025-02-10 16:54:15 +0700

    video 36

--- src/components/todo/todonew.jsx
+++ src/components/todo/todonew.jsx
@@ -1,19 +1,37 @@
 const TodoNew = (props) => {
   console.log("check point: ", props);
-  const { addNewTodo } = props;
-  addNewTodo("example");
+  //   const { addNewTodo } = props;
+  //   addNewTodo("example");
+  const handleClick = () => {
+    alert("click me");
+  };
+  //cách 1
+  //   const handleOnChange = (event) => {
+  //     console.log("handle onchange", event.target.value);
+  //   };
+  //cách 2
+  const handleOnChange = (name) => {
+    console.log("handle onchange", name);
+  };
   return (
     <form className="NewTodoForm" /*onSubmit={handleSubmit}*/>
       <label htmlFor="task">New todo</label>
       <input
         // value={userInput.task}
-        // onChange={handleChange}
+        //cách 1
+        // onChange={handleOnChange}
+        //cách 2
+        onChange={(event) => {
+          handleOnChange(event.target.value);
+        }}
         id="task"
         type="text"
         name="task"
         placeholder="New Todo"
       />
-      <button>Add Todo</button>
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add Todo
+      </button>
     </form>
   );
 };
